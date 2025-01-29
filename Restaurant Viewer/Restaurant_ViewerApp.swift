1a06443941028e6604235a1f0fe04b720a62cf4d
Siddharth M <56239073+Siddharthmr@users.noreply.github.com>
2025-01-28 20:36:56 -0600

    Initial Commit

---
+++ Restaurant Viewer/Restaurant_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ViewerApp.swift
+//  Restaurant Viewer
+//
+//  Created by Siddharth M on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restaurant ViewerTests/Restaurant_ViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ViewerTests.swift
+//  Restaurant ViewerTests
+//
+//  Created by Siddharth M on 1/28/25.
+//
+
+import Testing
+@testable import Restaurant_Viewer
+
+struct Restaurant_ViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
